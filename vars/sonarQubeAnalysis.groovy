b407732be314fbee4f011fe4ad76b34246535cb0
Ali khames <46407148+AliKhamed@users.noreply.github.com>
2024-06-10 12:30:43 +0300

    Update sonarQubeAnalysis.groovy

--- vars/sonarQubeAnalysis.groovy
+++ vars/sonarQubeAnalysis.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/env groovy
 
-def call(String sonarQubeCredentialsID, String SONAR_PROJECT_KEY, String sonarqubeUrl) {
+def call(String sonarTokenCredentialsID, String SONAR_PROJECT_KEY, String sonarqubeUrl) {
 
-    withCredentials([string(credentialsId: 'sonarQubeCredentialsID', variable: 'SONAR_TOKEN')]) {
+    withCredentials([string(credentialsId: 'sonarTokenCredentialsID', variable: 'SONAR_TOKEN')]) {
                         sh  """
                             ./gradlew sonar \
                             -Dsonar.projectKey=${SONAR_PROJECT_KEY} \
