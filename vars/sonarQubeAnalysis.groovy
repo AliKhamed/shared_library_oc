33917fe69cf463e305f796d1a53c1f659b0c304e
Alikhamed <alikhames566@gmail.com>
2024-06-10 17:54:27 +0300

    new

--- vars/sonarQubeAnalysis.groovy
+++ vars/sonarQubeAnalysis.groovy
@@ -1,15 +1,22 @@
 #!/usr/bin/env groovy
 
-def call(String sonarTokenCredentialsID, String SONAR_PROJECT_KEY, String sonarqubeUrl) {
-
-    withCredentials([string(credentialsId: 'sonarTokenCredentialsID', variable: 'SONAR_TOKEN')]) {
-                        sh  """
-                            ./gradlew sonar \
-                            -Dsonar.projectKey=${SONAR_PROJECT_KEY} \
-                            -Dsonar.host.url=${sonarqubeUrl} \
-                            -Dsonar.token=${SONAR_TOKEN} \
-                            -Dsonar.scm.provider=git \
-                            -Dsonar.java.binaries=build/classes
-                            """
-          }
+def call(){ 
+	withSonarQubeEnv() { 
+		    sh 'chmod +x gradlew'
+        	sh "./gradlew sonar" 
+	}
 }
+
+// def call(String sonarTokenCredentialsID, String SONAR_PROJECT_KEY, String sonarqubeUrl) {
+
+//     withCredentials([string(credentialsId: 'sonarTokenCredentialsID', variable: 'SONAR_TOKEN')]) {
+//                         sh  """
+//                             ./gradlew sonar \
+//                             -Dsonar.projectKey=${SONAR_PROJECT_KEY} \
+//                             -Dsonar.host.url=${sonarqubeUrl} \
+//                             -Dsonar.token=${SONAR_TOKEN} \
+//                             -Dsonar.scm.provider=git \
+//                             -Dsonar.java.binaries=build/classes
+//                             """
+//           }
+// }
