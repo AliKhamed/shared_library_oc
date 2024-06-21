752a9c82b0d4b582d5104e089059922dd53dfa16
Ali khames <46407148+AliKhamed@users.noreply.github.com>
2024-06-21 14:18:44 +0300

    Update editNewImage.groovy

--- vars/editNewImage.groovy
+++ vars/editNewImage.groovy
@@ -1,13 +1,13 @@
 #!/usr/bin/env groovy
 
-def call(String githubToken, String imageName, String gitUserEmail, String gitUserName, String gitRepoName) {
+def call(String githubToken, String imageName, String gitUserEmail, String gitUserName, String gitRepoName, String githubFilePath) {
 
     withCredentials([string(credentialsId: "${githubToken}", variable: 'GITHUB_TOKEN')]) {
                 sh '''
                     git config user.email "${gitUserEmail}"
                     git config user.name "${gitUserName}"
-                    sed -i "s|image:.*|image: ${imageName}:${BUILD_NUMBER}|g" k8s_manifest_files/deployment.yml
-                    git add k8s_manifest_files/deployment.yml
+                    sed -i "s|image:.*|image: ${imageName}:${BUILD_NUMBER}|g" ${githubFilePath}
+                    git add ${githubFilePath}
                     git commit -m "Update deployment image to version ${BUILD_NUMBER}"
                     git push https://${GITHUB_TOKEN}@github.com/${gitUserName}/${gitRepoName} HEAD:main
                    '''
